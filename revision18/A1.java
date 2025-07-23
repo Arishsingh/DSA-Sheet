346a8641868170b0e16c1299da1740ca91006916
अरिश सिंह <arishsingh9469@gmail.com>
2025-07-24 01:19:11 +0530

    Add files via upload

--- revision18/A1.java
+++ revision18/A1.java
@@ -1,3 +1,26 @@
-public class A1 {
+class node {
+    int data;
+    node next;
 
+    node(int value){
+        data = value;
+        next = null;
+    }
 }
+    public class A1{
+        public static void main(String[] args) {
+            node n1 = new node(1);
+            node n2 = new node(2);
+            node n3 = new node(3);
+            node n4 = new node(4);
+
+            node temp = n1;
+
+            while(temp != null){
+        System.out.println(temp.data);
+        temp = temp.next;
+            }
+            System.out.println("null");
+
+        }
+    }
